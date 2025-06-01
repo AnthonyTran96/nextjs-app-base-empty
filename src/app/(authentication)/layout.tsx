71cef046c256f119761053fac5376e5f6ebf1d80
AnthonyTran96 <quanghust96@gmail.com>
2025-06-01 11:08:12 +0700

    Enhance GitHub Actions workflow by adding SSH host key setup and removing StrictHostKeyChecking for secure connections. Comment out AuthGuard in layout component for potential debugging.

--- src/app/(authentication)/layout.tsx
+++ src/app/(authentication)/layout.tsx
@@ -1,13 +1,12 @@
 // PROJECT IMPORTS
 import AppLayout from 'layout/AppLayout';
-import AuthGuard from 'utils/route-guard/AuthGuard';
 
 // ==============================|| DASHBOARD LAYOUT ||============================== //
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <AuthGuard>
-      <AppLayout>{children}</AppLayout>
-    </AuthGuard>
+    // <AuthGuard>
+    <AppLayout>{children}</AppLayout>
+    // </AuthGuard>
   );
 }
